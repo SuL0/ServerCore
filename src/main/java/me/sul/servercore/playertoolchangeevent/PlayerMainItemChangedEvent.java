88e74fbc97c7fcf3a22973708a09789b87076144
SuL <jjw011202@naver.com>
2020-07-14 19:17:47 +0900

    PlayerMainItemChangeEvent -> PlayerMainItemChangedEvent

--- src/main/java/me/sul/servercore/playertoolchangeevent/PlayerMainItemChangedEvent.java
+++ src/main/java/me/sul/servercore/playertoolchangeevent/PlayerMainItemChangedEvent.java
@@ -5,14 +5,14 @@ import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.bukkit.inventory.ItemStack;
 
-public class PlayerMainItemChangeEvent extends Event { // 아이템을 바꾸지 않고, 핫바를 바꿔서 MainHand의 아이템이 바뀔 수도 있기 때문에 Inventory보다는 Player가 더 적합함.
+public class PlayerMainItemChangedEvent extends Event { // 아이템을 바꾸지 않고, 핫바를 바꿔서 MainHand의 아이템이 바뀔 수도 있기 때문에 Inventory보다는 Player가 더 적합함.
     private static final HandlerList handlers = new HandlerList();
     private Player player;
     private ItemStack clonedPreviousItem;         // ! 정확하지 않을 수 있음 !
     private ItemStack newItem;
 
 
-    public PlayerMainItemChangeEvent(Player player, ItemStack clonedPreviousItem, ItemStack newItem) {
+    public PlayerMainItemChangedEvent(Player player, ItemStack clonedPreviousItem, ItemStack newItem) {
         this.player = player;
         this.clonedPreviousItem = clonedPreviousItem;
         this.newItem = newItem;
