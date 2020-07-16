ec4c616b97ece6054682cb9129f556fc3b4bcef1
SuL <jjw011202@naver.com>
2020-07-17 03:34:30 +0900

    조합대에 모델링 아이템 넣는 방식 교체 (패킷 -> 진짜 아이템) updateInventory()를 할 시 패킷 아이템이 다 사라지는 버그가 있어서 교체하였음.

--- src/main/java/me/sul/servercore/inventorymodeling/InventoryModeling.java
+++ src/main/java/me/sul/servercore/inventorymodeling/InventoryModeling.java
@@ -1,186 +1,144 @@
 package me.sul.servercore.inventorymodeling;
 
 import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.ProtocolManager;
-import com.comphenix.protocol.events.PacketAdapter;
-import com.comphenix.protocol.events.PacketContainer;
-import com.comphenix.protocol.events.PacketEvent;
-import com.comphenix.protocol.wrappers.EnumWrappers;
 import me.sul.servercore.ServerCore;
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
-import org.bukkit.Server;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
+import me.sul.servercore.playertoolchangeevent.InventoryItemChangedEvent;
+import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ItemSpawnEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.inventory.InventoryView;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.ShapelessRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 
 // 원본 코드 출처 : https://github.com/AgentTroll/static-player-inv/blob/master/src/main/java/com/gmail/woodyc40/staticplayerinv/Main.java
+// NOTE: 패킷으로 조합대에 템을 넣는 방법은 p.updateInventory()를 하면 아이템이 다 날아감. 근데 1초마다 자동 update되는건 안그럼.
 public class InventoryModeling implements Listener {
     private static final int PLAYER_CRAFT_INV_SIZE = 5;
-    private static final long INV_UPDATE_INTERVAL = 20L;
-    private Map<Player, BukkitTask> itemRetentionTasks =
-            new HashMap<>();
     private final ItemStack INVENTORY_MODELING_ITEM;
     private final ItemStack BUTTON1;
     private final ItemStack BUTTON2;
     private final ItemStack BUTTON3;
     private final ItemStack BUTTON4;
 
-    private ProtocolManager pm = ProtocolLibrary.getProtocolManager();
-
     public InventoryModeling() {
-        INVENTORY_MODELING_ITEM = new ItemStack(Material.GOLD_HOE, 1, (short)16);
-
-        BUTTON1 = new ItemStack(Material.STONE);
-        ItemStack button2 = new ItemStack(Material.STONE);
+        // 아이템 추가
+        BUTTON1 = new ItemStack(Material.GOLD_HOE);
+        ItemStack button2 = new ItemStack(Material.GOLD_HOE);
         ItemMeta button2Meta = button2.getItemMeta();
         button2Meta.setDisplayName("2번 버튼");
         button2.setItemMeta(button2Meta);
         BUTTON2 = button2;
 
-        BUTTON3 = new ItemStack(Material.STONE);
-        ItemStack button4 = new ItemStack(Material.STONE);
+        BUTTON3 = new ItemStack(Material.GOLD_HOE);
+        ItemStack button4 = new ItemStack(Material.GOLD_HOE);
         ItemMeta button4Meta = button4.getItemMeta();
         button4Meta.setDisplayName("4번 버튼");
         button4.setItemMeta(button4Meta);
         BUTTON4 = button4;
+
+        INVENTORY_MODELING_ITEM = new ItemStack(Material.GOLD_HOE, 1, (short)16);
+
+        // 레시피 추가
+        NamespacedKey invModelingKey = new NamespacedKey(ServerCore.getInstance(), "inventory_modeling_item");
+        ShapelessRecipe invModelingRecipe = new ShapelessRecipe(invModelingKey, INVENTORY_MODELING_ITEM);
+        invModelingRecipe.addIngredient(1, BUTTON1.getType());
+        invModelingRecipe.addIngredient(1, BUTTON2.getType());
+        invModelingRecipe.addIngredient(1, BUTTON3.getType());
+        invModelingRecipe.addIngredient(1, BUTTON4.getType());
+        Bukkit.addRecipe(invModelingRecipe);
     }
 
     @EventHandler
-    public void onJoin(PlayerJoinEvent event) {
-        Player player = event.getPlayer();
-        BukkitTask task = Bukkit.getScheduler().runTaskTimer(ServerCore.getInstance(), () -> {
-            InventoryView view = player.getOpenInventory();
-            if (isPlayerInv(view)) {  // 인벤 제외한 GUI열면 false
-                sendInventoryModelingItemPacket(player);
-            }
-        }, 0L, INV_UPDATE_INTERVAL/5);
-        this.itemRetentionTasks.put(player, task);
-
+    public void onJoin(PlayerJoinEvent e) {
+        Player p = e.getPlayer();
         Bukkit.getScheduler().runTaskLater(ServerCore.getInstance(), () -> {
-            sendButtonItemPacket(player);
-        }, 1);
-
-
+            addButtonToCraftingTable(p);
+        }, 1L);
     }
 
-    private void sendInventoryModelingItemPacket(Player player) { // 인벤토리 닫겨있을 때 패킷을 아무리 넣어봐야 의미가 없어서, 주기를 짧게 줘야 함
-        PacketContainer packet = pm.createPacket(PacketType.Play.Server.SET_SLOT);
-        packet.getIntegers().write(0, 0).write(1, 0);
-        packet.getItemModifier().write(0, INVENTORY_MODELING_ITEM);
-        try {
-            pm.sendServerPacket(player, packet);
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
+    // NOTE: 인벤을 여는건 감지가 안되는데, 닫는건 감지가 됨.
+    @EventHandler
+    public void onClose(InventoryCloseEvent e) {
+        Player p = (Player) e.getPlayer();
+        InventoryView view = e.getView();
+        if (isPlayerCraftingInv(view)) {
+            view.getTopInventory().clear();
+            // 조합대에 다시 아이템 넣어주기
+            Bukkit.getScheduler().runTaskLater(ServerCore.getInstance(), () -> { // 1틱안에 다른 인벤을 열거나, 죽었을 시 추후의 onRespawn/onClose에서 처리하게 돼 있음.
+                Bukkit.getServer().broadcastMessage("SETSLOT - onClose");
+                addButtonToCraftingTable(p);
+            }, 1L);
         }
     }
-    private void sendButtonItemPacket(Player player) { // CraftingTable의 4칸슬롯에 packet을 보내면 '죽지 않는 한' 그대로 있음. 심지어 게임모드를 바꾸더라도.
-        for (int slot=1; slot<=4; slot++) {
-            PacketContainer packet = pm.createPacket(PacketType.Play.Server.SET_SLOT);
-            packet.getIntegers().write(0, 0).write(1, slot);
-            if (slot==1)
-                packet.getItemModifier().write(0, BUTTON1);
-            else if (slot==2)
-                packet.getItemModifier().write(0, BUTTON2);
-            else if (slot==3)
-                packet.getItemModifier().write(0, BUTTON3);
-            else if (slot==4)
-                packet.getItemModifier().write(0, BUTTON4);
-            try {
-                pm.sendServerPacket(player, packet);
-            } catch (InvocationTargetException e) {
-                e.printStackTrace();
+    @EventHandler
+    public void onRespawn(PlayerRespawnEvent e) {
+        addButtonToCraftingTable(e.getPlayer());
+    }
+
+    @EventHandler
+    public void onClick(InventoryClickEvent e) {
+        InventoryView view = e.getView();
+        if (isPlayerCraftingInv(view) && e.getClickedInventory() != e.getWhoClicked().getInventory()) {
+            if (e.getSlot() < 5) {
+                e.setCancelled(true);
             }
         }
     }
 
-    @EventHandler
-    public void onQuit(PlayerQuitEvent event) {
-        Player player = event.getPlayer();
-        BukkitTask task = this.itemRetentionTasks.remove(player);
-        if (task != null) {
-            task.cancel();
+
+
+    private void addButtonToCraftingTable(Player p) {
+        InventoryView view = p.getOpenInventory();
+        if (!p.isDead() && isPlayerCraftingInv(view)) { // 죽을 때 inventoryClose가 실행돼서 isDead도 체크해야 함
+            view.setItem(1, BUTTON1);
+            view.setItem(2, BUTTON2);
+            view.setItem(3, BUTTON3);
+            view.setItem(4, BUTTON4);
         }
     }
 
-    @EventHandler
-    public void onClick(InventoryClickEvent event) {
-        InventoryView view = event.getView();
-        Player player = (Player) event.getWhoClicked();
-        if (isPlayerInv(view)) {
-            if (event.getClickedInventory() != player.getInventory()) {
-                InventoryAction invAction = event.getAction();
-                if (invAction == InventoryAction.PLACE_ALL || invAction == InventoryAction.PLACE_SOME ||
-                        invAction == InventoryAction.PLACE_ONE || invAction == InventoryAction.HOTBAR_SWAP) event.setCancelled(true);
-
-                // NOTE: 크래프팅 테이블 5칸이 한 몸처럼 작동해서 하나 클릭하면 5개 다 템이 사라짐
-                Bukkit.getScheduler().runTaskLater(ServerCore.getInstance(), () -> {
-                    InventoryView view2 = player.getOpenInventory();
-                    if (isPlayerInv(view2)) {
-                        sendInventoryModelingItemPacket(player);
-                        sendButtonItemPacket(player);
-                    }
-                }, 1L);
-            } else {
-                if (event.getSlot() >= 36 && event.getSlot() <= 39) {
-                    Bukkit.getScheduler().runTaskLater(ServerCore.getInstance(), () -> {
-                        InventoryView view2 = player.getOpenInventory();
-                        if (isPlayerInv(view2)) {
-                            sendInventoryModelingItemPacket(player);
-                        }
-                    }, 1L);
-                } else if (event.getSlot() == 40) {
-                    Bukkit.getScheduler().runTaskLater(ServerCore.getInstance(), () -> {
-                        InventoryView view2 = player.getOpenInventory();
-                        if (isPlayerInv(view2)) {
-                            sendInventoryModelingItemPacket(player);
-                            sendButtonItemPacket(player);
-                        }
-                    }, 1L);
-                }
-            }
-        }
-    }
-    @EventHandler
-    public void onDeath(PlayerRespawnEvent e) {
-        sendButtonItemPacket(e.getPlayer());
-    }
-
-    private static boolean isPlayerInv(InventoryView view) {
+    private static boolean isPlayerCraftingInv(InventoryView view) {
         return view.getTopInventory().getSize() == PLAYER_CRAFT_INV_SIZE;
     }
 
 
-//    // https://www.spigotmc.org/threads/detect-inventory-open-e.257205/ -> 1.12에선 Achievement가 사라졌다.
-//    private void addInventoryOpenListener() {
-//        ProtocolLibrary.getProtocolManager().addPacketListener(new PacketAdapter(ServerCore.getInstance(), PacketType.Play.Client.CLIENT_COMMAND){
-//            @Override
-//            public void onPacketReceiving(PacketEvent event) {
-//
-//                if (event.getPacket().getClientCommands().read(0) == EnumWrappers.ClientCommand.OPEN_INVENTORY_ACHIEVEMENT){
-//                    Player player = event.getPlayer();
-//                    Bukkit.getServer().broadcastMessage("inventory open -> " + player);
-//                }
-//            }
-//        });
-//    }
+
+    /*
+     버그 방지
+    */
+    @EventHandler
+    public void onItemSpawn(ItemSpawnEvent e) {
+        ItemStack is = e.getEntity().getItemStack();
+        if (is.equals(BUTTON1) || is.equals(BUTTON2) || is.equals(BUTTON3) || is.equals(BUTTON4)) {
+            // TODO: 아이템 스폰 취소 로그 추가
+            Bukkit.getServer().broadcastMessage("§4[심각] BUTTON 드랍이 시도됨.");
+            e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onItemAdded(InventoryItemChangedEvent e) {
+        ItemStack is = e.getItemStack();
+        if (is.equals(BUTTON1) || is.equals(BUTTON2) || is.equals(BUTTON3) || is.equals(BUTTON4)) {
+            // TODO: 아이템 획득 취소 로그 추가
+            Bukkit.getServer().broadcastMessage("§4[심각] " + e.getPlayer().getDisplayName() + "§4에게서 BUTTON 획득이 시도됨.");
+            e.getPlayer().getInventory().setItem(e.getSlot(), new ItemStack(Material.AIR));
+        }
+    }
 }
