b0674021d771283f05a8bb7fb464f1761bc25b65
SuL <jjw011202@naver.com>
2020-07-24 20:16:56 +0900

    날씨 관리도 하게끔 추가

--- src/main/java/me/sul/servercore/timeandweathermanager/TimeAndWeatherManager.java
+++ src/main/java/me/sul/servercore/timeandweathermanager/TimeAndWeatherManager.java
@@ -1,19 +1,21 @@
-package me.sul.servercore.timemanager;
+package me.sul.servercore.timeandweathermanager;
 
 import me.sul.servercore.ServerCore;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 
-public class TimeManager {
+public class TimeAndWeatherManager {
     // 06:00 이 0틱. 1시간당 1000틱. 전체 틱은 24000.
     // 08:00 ~ 16:00 으로 고정
-    public TimeManager() {
+    public TimeAndWeatherManager() {
         Bukkit.getScheduler().runTaskTimer(ServerCore.getInstance(), () -> {
             for (World world : Bukkit.getServer().getWorlds()) {
                 long time = world.getTime();
                 if (!(time >= 2000 && time <= 10000)) {
                     world.setTime(2000);
                 }
+                if (world.hasStorm()) world.setStorm(false);
+                if (world.isThundering()) world.setThundering(false);
             }
         }, 0L, 40*20L);
     }
