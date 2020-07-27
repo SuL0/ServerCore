e7ca69baa0870d63c2b9a15157899ece71b8264c
SuL <jjw011202@naver.com>
2020-07-27 19:37:32 +0900

    핫바의 슬롯과, 크래프팅 슬롯이 둘 다 0~4번 슬롯을 사용하는 버그를 수정 Crafting Result: 41 Crafting Table: 42~45

--- src/main/java/me/sul/servercore/playertoolchangeevent/InventorySlotConverterUtil.java
+++ src/main/java/me/sul/servercore/playertoolchangeevent/InventorySlotConverterUtil.java
@@ -3,7 +3,9 @@ package me.sul.servercore.playertoolchangeevent;
 public class InventorySlotConverterUtil {
     public static int nmsSlotToSpigotSlot(int nmsSlot) {
         int spigotSlot = nmsSlot;
-        if (nmsSlot >= 5 && nmsSlot <= 8) {  // 갑옷칸
+        if (nmsSlot >= 0 && nmsSlot <= 4) { // NOTE: 조합대 칸. 0: Result ... -> 41: Result, 42, 43, 44, 45로 설정
+            spigotSlot = nmsSlot + 41;
+        }  else if (nmsSlot >= 5 && nmsSlot <= 8) {  // 갑옷칸
             if (nmsSlot == 5) { spigotSlot = 39; }
             else if (nmsSlot == 6) { spigotSlot = 38; }
             else if (nmsSlot == 7) { spigotSlot = 37; }
