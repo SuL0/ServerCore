9d42915444538af28b9e37f8435f0a0d13bd35c1
SuL <jjw011202@naver.com>
2020-08-13 21:02:31 +0900

    아이템 드랍 시도 시 mainItemOfPlayers의 아이템스택이 AIR로 변경되는 치명적인 버그 수정

--- src/main/java/me/sul/servercore/inventoryevent/InventoryItemListener.java
+++ src/main/java/me/sul/servercore/inventoryevent/InventoryItemListener.java
@@ -1,18 +1,14 @@
 package me.sul.servercore.inventoryevent;
 
-import me.sul.crackshotaddition.CrackShotAddition;
 import me.sul.servercore.serialnumber.UniqueIdAPI;
 import net.minecraft.server.v1_12_R1.*;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
-import org.bukkit.craftbukkit.v1_12_R1.inventory.CraftInventory;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.player.PlayerItemHeldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -21,7 +17,7 @@ import org.bukkit.inventory.ItemStack;
 import java.util.HashMap;
 
 public class InventoryItemListener implements Listener {
-    HashMap<Player, ItemStack> mainItemOfPlayers = new HashMap<>();
+    HashMap<Player, ItemStack> clonedMainItemOfPlayers = new HashMap<>();
 
 
     @EventHandler(priority = EventPriority.LOWEST)
@@ -37,15 +33,14 @@ public class InventoryItemListener implements Listener {
             public void a(Container container, int nmsSlot, net.minecraft.server.v1_12_R1.ItemStack nmsIs) {
                 int newSlot = InventorySlotConverterUtil.nmsSlotToSpigotSlot(nmsSlot);
                 ItemStack newIs = p.getInventory().getItem(newSlot) != null ? p.getInventory().getItem(newSlot) : new ItemStack(Material.AIR);
-//                ItemStack newIs = CraftItemStack.asBukkitCopy(nmsIs); // 이렇게 하면 clone된거라서 아이템의 ItemMeta를 수정하기가 번거로워짐.
 
-                // "슬롯을 바꾸지 않은 채로" 손에 든 아이템이 바꼈을 때. 아이템 정보 바뀌는건 제외(시리얼번호 비교. 없다면 그냥 계속 중복되게 호출됨)
+                // "슬롯을 바꾸지 않은 채로" 손에 든 아이템이 바꼈을 때. 아이템 정보 바뀌는건 제외(UID 비교. 없다면 그냥 계속 중복되게 호출됨)
                 if (newSlot == p.getInventory().getHeldItemSlot()) {
-                    ItemStack clonedPreviousMainIs = mainItemOfPlayers.containsKey(p) ? mainItemOfPlayers.get(p) : new ItemStack(Material.AIR);
+                    ItemStack clonedPreviousMainIs = clonedMainItemOfPlayers.containsKey(p) ? clonedMainItemOfPlayers.get(p) : new ItemStack(Material.AIR);
                     if (!(UniqueIdAPI.hasUniqueID(clonedPreviousMainIs) && UniqueIdAPI.hasUniqueID(newIs) &&
                             UniqueIdAPI.getUniqueID(clonedPreviousMainIs).equals(UniqueIdAPI.getUniqueID(newIs)))) {
                         Bukkit.getPluginManager().callEvent(new PlayerMainItemChangedConsideringUidEvent(p, clonedPreviousMainIs, newIs, newSlot)); // EVENT: Call PlayerMainItemChangeEvent
-                        mainItemOfPlayers.put(p, newIs);
+                        clonedMainItemOfPlayers.put(p, newIs.clone());
                     }
                 }
                 Bukkit.getPluginManager().callEvent(new InventoryItemChangedEvent(p, newSlot, newIs)); // EVENT: Call InventoryItemChangedEvent
@@ -68,7 +63,8 @@ public class InventoryItemListener implements Listener {
         if (clonedPreviousIs.getType().equals(Material.AIR) && newIs.getType().equals(Material.AIR)) return;
 
         Bukkit.getPluginManager().callEvent(new PlayerMainItemChangedConsideringUidEvent(p, clonedPreviousIs, newIs, e.getNewSlot()));
-        mainItemOfPlayers.put(p, newIs);
+        clonedMainItemOfPlayers.put(p, newIs.clone()); // NOTE: 씨발 Drop이벤트가 취소되든 말든간에 ItemStack이 AIR로 바뀌는 버그때문에 .clone해서 저장해야 함.
+                                                       // NOTE: 근데 어차피 상관없는게, 정상적으로 아이템이 바꼈으면 이벤트에 걸려서 수정되게 돼있음.
     }
 
 
@@ -118,6 +114,6 @@ public class InventoryItemListener implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onQuit(PlayerQuitEvent e) {
-        mainItemOfPlayers.remove(e.getPlayer());
+        clonedMainItemOfPlayers.remove(e.getPlayer());
     }
 }
