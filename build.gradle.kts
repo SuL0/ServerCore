a54a6ae5f53e7417376e67f770ea7fd57272fa75
SuL <jjw011202@naver.com>
2021-08-02 23:49:41 +0900

    여러 라이브러리를 그냥 모두 ServerCore에 모두 Shade 함 (굳이 라이브러리 플긴 때문에 플러그인 폴더를 어지럽히고 싶지 않기 때문)

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ repositories {
     maven("https://papermc.io/repo/repository/maven-public/")
     maven("https://repo.dmulloy2.net/nexus/repository/public/")
     maven("https://repo.codemc.io/repository/maven-snapshots")
+    maven("http://maven.elmakers.com/repository/")
     mavenLocal()
 }
 
@@ -19,17 +20,21 @@ dependencies {
     compileOnly("com.destroystokyo.paper", "paper-api", "1.12.2-R0.1-SNAPSHOT")
     compileOnly("org.spigotmc", "spigot", "1.12.2-R0.1-SNAPSHOT")
 
-    compileOnly("com.comphenix.protocol", "ProtocolLib", "4.5.1")
-    compileOnly("net.wesjd", "anvilgui", "1.5.0-SNAPSHOT")
-    compileOnly("net.lingala.zip4j", "zip4j", "2.7.0")
-    runtimeOnly("net.lingala.zip4j", "zip4j", "2.7.0")
-    compileOnly("net.luckperms", "api", "5.3")
-    compileOnly("xyz.upperlevel.spigot.book", "spigot-book-api", "1.5")
-    compileOnly("net.citizensnpcs", "citizensapi", "2.0.28-SNAPSHOT")
-    compileOnly("com.github.MilkBowl","VaultAPI", "1.7")
-
     compileOnly(files("$pluginStorage/CrackShotAddition_S.jar"))
     compileOnly(files("$pluginStorage/Dependencies/item-nbt-api-plugin-2.6.0.jar"))
+    
+    
+    // SHADE
+    compileOnly("net.wesjd", "anvilgui", "1.5.0-SNAPSHOT")
+    compileOnly("net.lingala.zip4j", "zip4j", "2.7.0")
+//    runtimeOnly("net.lingala.zip4j", "zip4j", "2.7.0")
+    compileOnly("xyz.upperlevel.spigot.book", "spigot-book-api", "1.5")
+    compileOnly("com.github.MilkBowl","VaultAPI", "1.7")
+    compileOnly("com.elmakers.mine.bukkit","EffectLib", "9.0")
+    compileOnly("com.comphenix.protocol", "ProtocolLib", "4.6.0")
+    compileOnly("net.luckperms", "api", "5.3")
+    compileOnly("net.citizensnpcs", "citizensapi", "2.0.28-SNAPSHOT")
+    compileOnly(files("$pluginStorage/ResourcepackSoundPlayer_S.jar"))
 }
 
 val shade = configurations.create("shade")
@@ -56,7 +61,13 @@ tasks {
         from(
             shade.filter { it.name.startsWith("anvilgui")
                     || it.name.startsWith("zip4j")
-                    || it.name.startsWith("spigot-book-api") }
+                    || it.name.startsWith("spigot-book-api")
+                    || it.name.startsWith("VaultAPI")
+                    || it.name.startsWith("EffectLib")
+                    || it.name.startsWith("ResourcepackSoundPlayer")
+                    || it.name.startsWith("luckperms")
+                    || it.name.startsWith("citizensapi")
+                    || it.name.startsWith("ProtocolLib") }
                 .map {
                     if (it.isDirectory)
                         it
