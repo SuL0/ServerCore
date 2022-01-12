901fccb7faea2f1f1d8aa98dfdf87756fda1e86e
SuL <sul0332u@gmail.com>
2022-01-12 21:06:37 +0900

    NbtItem(private val item -> val item)

--- src/main/kotlin/kr/sul/servercore/nbtapi/NbtItem.kt
+++ src/main/kotlin/kr/sul/servercore/nbtapi/NbtItem.kt
@@ -4,7 +4,7 @@ import net.minecraft.server.v1_12_R1.NBTTagCompound
 import org.bukkit.craftbukkit.v1_12_R1.inventory.CraftItemStack
 import org.bukkit.inventory.ItemStack
 
-class NbtItem(private val item: ItemStack) {
+class NbtItem(val item: ItemStack) {
     private val nmsItem = CraftItemStack.asNMSCopy(item)
     val tag = if (nmsItem.hasTag()) nmsItem.tag!! else NBTTagCompound()
 
