9b538413892146f057845801baaf3696144b1767
SuL <sul0332u@gmail.com>
2022-09-20 13:09:41 +0900

    Bungee 서브모듈 추가

---
+++ Bungee/build.gradle.kts
@@ -0,0 +1,18 @@
+group = "kr.sul"
+version = ext.get("version")!!
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven("https://papermc.io/repo/repository/maven-public/")
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    compileOnly(kotlin("stdlib-jdk8"))
+    compileOnly("io.github.waterfallmc:waterfall-api:1.18-R0.1-SNAPSHOT")
+    implementation("org.redisson:redisson:3.17.6")
+    implementation("io.github.revxrsal:bungee:3.0.8")
+    implementation("io.github.revxrsal:common:3.0.8")
+    implementation("com.zaxxer:HikariCP:5.0.1")
+}
---
+++ Bungee/src/main/kotlin/kr/sul/servercore/ServerCore.kt
@@ -0,0 +1,6 @@
+package kr.sul.servercore
+
+import net.md_5.bungee.api.plugin.Plugin
+
+class ServerCore: Plugin() {
+}
